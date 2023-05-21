484c70fe6f9e3271c15fd4421f38e434d5b7325a
Joao Vitor Alves Soares <joao.soares98@icloud.com>
2023-05-21 20:59:03 -0300

    feat: add multer and createProduct useCase

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'node:path';
 import mongoose from 'mongoose';
 
 import { router } from './router';
@@ -17,6 +18,7 @@ mongoose
     const app = express();
     const PORT = 3001;
 
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,24 @@
+import path from 'node:path';
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
+import { listProducts } from './app/useCases/products/listProducts';
+import { createProduct } from './app/useCases/products/createProduct';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    }
+  }),
+});
 
 router.get('/categories', listCategories);
 
@@ -13,13 +28,9 @@ router.get('/categories/:categoryId/products', (req, res) => {
   res.send('OK');
 });
 
-router.get('/products', (req, res) => {
-  res.send('OK');
-});
+router.get('/products', listProducts);
 
-router.post('/products', (req, res) => {
-  res.send('OK');
-});
+router.post('/products', upload.single('image'), createProduct);
 
 router.get('/orders', (req, res) => {
   res.send('OK');
