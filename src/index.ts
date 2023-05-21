b54d47cba8f3d00e9e871e05c3715750eec0686f
Joao Vitor Alves Soares <joao.soares98@icloud.com>
2023-05-20 23:26:28 -0300

    feat: add docker and mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://waiterapp:waiterapp@localhost:27017/')
+  .then(() => {
+    const app = express();
+    const PORT = 3001;
 
-const PORT = 3001;
-
-app.listen(3001, () =>
-  console.log(`Server is running at http://localhost:${PORT}`));
+    app.listen(3001, () =>
+      console.log(`Server is running at http://localhost:${PORT}`)
+    );
+  })
+  .catch(() => console.error('Error while connect to MongoDB'));
