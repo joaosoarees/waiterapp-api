3dfdda9c5b0ec2514f7fdde67d2f1401415cc6b3
Joao Vitor Alves Soares <joao.soares98@icloud.com>
2023-05-21 00:38:10 -0300

    refactor: add try catch to createCategory use case

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -3,12 +3,17 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(req: Request, res: Response) {
-  const { icon, name } = req.body;
+  try {
+    const { icon, name } = req.body;
 
-  const category = await Category.create({
-    icon,
-    name
-  });
+    const category = await Category.create({
+      icon,
+      name
+    });
 
-  res.json(category);
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 }
